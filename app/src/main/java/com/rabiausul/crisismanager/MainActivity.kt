07216b86dc5ca4fa2ba28a891ed20f16291a21f8
Rabia Usul <rabiausul@Rabia-MacBook-Air.local>
2026-04-24 21:33:24 +0300

    first commit

---
+++ app/src/main/java/com/rabiausul/crisismanager/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.rabiausul.crisismanager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rabiausul.crisismanager.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+    fun login(view: View){
+        binding.textView2.text = "hayoooooo"
+    }
+}
