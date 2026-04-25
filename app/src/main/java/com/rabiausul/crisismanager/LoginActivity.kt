2efb7fa93c06ceddc4642ff73f64d7072cfda52a
Rabia Usul <rabiausul@Rabia-MacBook-Air.local>
2026-04-25 15:18:28 +0300

    second commit

--- app/src/main/java/com/rabiausul/crisismanager/LoginActivity.kt
+++ app/src/main/java/com/rabiausul/crisismanager/LoginActivity.kt
@@ -1,27 +1,31 @@
 package com.rabiausul.crisismanager
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.rabiausul.crisismanager.databinding.ActivityMainBinding // Binding importu
+import com.rabiausul.crisismanager.databinding.ActivityLoginBinding
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
+        setContentView(binding.root) // Ekranı binding ile set ediyoruz
 
+        // findViewById(R.id.main) yerine binding.root kullanarak hatayı çözüyoruz
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.textView6.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/rabiausul/crisismanager/RegisterActivity.kt
+++ app/src/main/java/com/rabiausul/crisismanager/RegisterActivity.kt
@@ -1,20 +1,23 @@
 package com.rabiausul.crisismanager
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.rabiausul.crisismanager.databinding.ActivityMainBinding // Binding importu
+import com.rabiausul.crisismanager.databinding.ActivityLoginBinding
+import com.rabiausul.crisismanager.databinding.ActivityRegisterBinding
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding : ActivityRegisterBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
         setContentView(binding.root)
 
@@ -22,6 +25,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.textView8.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 }
